a20582633575e8e3ffa2357684926df32cf4d0f7
Hady Eslam <30216972+Hady-Eslam@users.noreply.github.com>
2018-05-07 16:37:44 +0200

    Add files via upload

---
+++ Version 3.0/Source Code/Begining.java
@@ -0,0 +1,13 @@
+package algorithm_project;
+
+import java.awt.EventQueue;
+
+public class Begining {
+
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(() -> {
+            new Main();
+        });
+    }    
+}
